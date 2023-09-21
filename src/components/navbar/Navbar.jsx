b2231fabf38bfa50a84301bfbe0bd42f7982ed31
emirbeknurbekov <emirbek.nurbekov@alatoo.edu.kg>
2023-09-21 10:48:28 +0600

    add nav and footer

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+import "./Navbar.css";
+import Logo from "../../icons/Logo.svg";
+
 const Navbar = () => {
-  return <div>Navbar </div>;
+  return (
+    <div id="navbar">
+      <div className="navbar__container">
+        <div className="navbar__logo">
+          <img src={Logo} alt="error" />
+        </div>
+        <ul className="navbar__titles">
+          <li className="navbar__item">Home</li>
+          <li className="navbar__item">Employer</li>
+          <li className="navbar__item">Candidate</li>
+        </ul>
+        <div className="navbar__auth__block">
+          <button className="navbar__login">Login</button>
+          <button className="navbar__register">Sign Up</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
