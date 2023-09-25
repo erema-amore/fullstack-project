ec2f3949a929a9a87dd1ca79566d6f6eb648135b
erema-amore <erema.amore@mail.ru>
2023-09-26 00:36:20 +0600

    creat post list and redact register

--- src/components/recruiter/RecruiterRegister.jsx
+++ src/components/recruiter/RecruiterRegister.jsx
@@ -4,6 +4,9 @@ import { useNavigate } from 'react-router-dom';
 import { clearStatus } from '../../store/recruiter/recruiterSlice';
 import { registerRecruiter } from '../../store/recruiter/recruiterAction';
 import styles from './recruiterRegister.module.css'; 
+import cosmos from '../../img/Recruiter/RecruiterLogin/cosmos.svg'
+import rupor from '../../img/Recruiter/RecruiterLogin/rupor.svg'
+import paper from '../../img/Recruiter/RecruiterLogin/paper.svg'
 
 
 const RecruiterRegister = () => {
@@ -22,6 +25,45 @@ const RecruiterRegister = () => {
 
   return (
     <div className={styles.main}>
+        <div className={styles.block_1_main}>
+       <div className={styles.block_1}>  
+          <p className={styles.block_1_txt_1}>Post your jobs where millions of qualified technology professionals are searching.</p>
+          <p className={styles.block_1_txt_2}>Dice is a hiring platform that only does tech. If you’re hiring engineers, developers, analysts or technicians, you need to be on Dice.</p>
+        </div>
+        </div>
+        <div className={styles.block_2_main}>
+        <div className={styles.block_2}>
+
+          <div className={styles.block_2_first}>
+
+          <div className={styles.block_2_first_main}>
+            <div> <img className={styles.block_2_main_img} src={ cosmos } alt="cosmos" /> </div>
+            <div>
+              <p className={styles.block_2_main_txt_1}>Fill tech jobs fast</p>
+              <p className={styles.block_2_main_txt_2}>The Dice community is expansive, diverse and growing.</p>
+              <p className={styles.block_2_main_txt_2}>• Over 5.9M Dice Members</p>
+              <p className={styles.block_2_main_txt_2}>• 49K new members per month</p>
+            </div>
+          </div>
+
+          <div className={styles.block_2_first_main}>
+            <div> <img className={styles.block_2_main_img} src={ rupor } alt="cosmos" /> </div>
+            <div>
+              <p className={styles.block_2_main_txt_1}>Access tech talent you can't find anywhere else</p>
+              <p className={styles.block_2_main_txt_2}>83% of active tech professionals on Dice aren’t actively searching for jobs on Indeed, ZipRecruiter, Career Builder & Monster.</p>
+              
+            </div>
+          </div>
+
+          <div className={styles.block_2_first_main}>
+            <div> <img className={styles.block_2_main_img}  src={ paper } alt="cosmos" /> </div>
+            <div>
+              <p className={styles.block_2_main_txt_1}>More qualified applicants for each jobt</p>
+              <p className={styles.block_2_main_txt_2}>715K job applications per month are submitted on Dice.</p>
+             
+            </div>
+          </div>
+          </div>
       
     <div className={styles.container}>
       <h3 className={styles.heading}>Регистрация</h3>
@@ -31,8 +73,12 @@ const RecruiterRegister = () => {
           <button className={styles.button} onClick={() => dispatch(clearStatus())}>Попробовать снова</button>
         </>
       ) : (
+        
         <div>
+        
+      
 
+        <div>
           
           <input type="email" 
            className={styles.inputField}
@@ -63,7 +109,16 @@ const RecruiterRegister = () => {
             Зарегистрироваться
           </button>
         </div>
+
+        
+
+        </div>
+
+       
+
       )}
+    </div>
+    </div>
     </div>
     </div>
   );
