84d903e798c5a53a57c3785449d76a53d4b9aa52
JildizK <jildizk13@gmail.com>
2023-09-26 14:39:56 +0600

    user&recruiter reg/log/logout/profile/profileUpfate done

--- src/components/profile/UserProfile.jsx
+++ src/components/profile/UserProfile.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect }from 'react'
+import React, { useEffect}from 'react'
 import { useDispatch, useSelector} from 'react-redux';
 import { getOneUser } from '../../store/user/userAction';
 import { useNavigate } from 'react-router-dom';
 
 const UserProfile = () => {
-  const { profile } = useSelector(state => state.user)
-console.log(profile);
+const { profile } = useSelector(state => state.user)
 
 const dispatch = useDispatch()
 const navigate = useNavigate()
@@ -14,14 +13,16 @@ useEffect(() => {
   dispatch(getOneUser());
 }, []);
 
+const fileName = profile.user_resume ? profile.user_resume.split('/').pop() : "";
+
   return (
     <div>
-  <img src={profile.image} alt={profile.name} width="50" height="50" />
+  <img src={`http://35.224.14.145${profile.image}`} alt={profile.name} width="50" height="50" />
   <h3>User Name: {profile.name}</h3>
   <h4>User Surname: {profile.surname}</h4>
   <h4>User contact email: {profile.email}</h4>
   <h4>Summery/objectives: {profile.about_user}</h4>
-  <iframe src={profile.user_resume} width="50%" height="250px"></iframe>
+  <h4>Resume:  {fileName}</h4>
   <button onClick={() => navigate("/profile-update/user/") }>Update Profile</button>
 
     </div>
