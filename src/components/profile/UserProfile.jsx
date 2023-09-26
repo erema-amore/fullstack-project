fb3bc2df75b922c618c77d69d1d3591ebf190ca1
erema-amore <erema.amore@mail.ru>
2023-09-26 14:52:12 +0600

    reister login

--- src/components/profile/UserProfile.jsx
+++ src/components/profile/UserProfile.jsx
@@ -2,30 +2,74 @@ import React, { useEffect }from 'react'
 import { useDispatch, useSelector} from 'react-redux';
 import { getOneUser } from '../../store/user/userAction';
 import { useNavigate } from 'react-router-dom';
+import styles from './userProfile.module.css'; 
 
 const UserProfile = () => {
   const { profile } = useSelector(state => state.user)
-console.log(profile);
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-const dispatch = useDispatch()
-const navigate = useNavigate()
-
-useEffect(() => {
-  dispatch(getOneUser());
-}, []);
+  useEffect(() => {
+    dispatch(getOneUser());
+  }, []);
 
   return (
-    <div>
-  <img src={profile.image} alt={profile.name} width="50" height="50" />
-  <h3>User Name: {profile.name}</h3>
-  <h4>User Surname: {profile.surname}</h4>
-  <h4>User contact email: {profile.email}</h4>
-  <h4>Summery/objectives: {profile.about_user}</h4>
-  <iframe src={profile.user_resume} width="50%" height="250px"></iframe>
-  <button onClick={() => navigate("/profile-update/user/") }>Update Profile</button>
-
+    <div className={styles.main}>
+    <div className={styles.container}> 
+      <img className={styles.profileImage} src={profile.image} alt={profile.name} width="50" height="50" />
+      <h3 className={styles.heading}>User Name: {profile.name}</h3>
+      <h4 className={styles.userInfo}>User Surname: {profile.surname}</h4>
+      <h4 className={styles.userInfo}>User contact email: {profile.email}</h4>
+      <h4 className={styles.userInfo}>Summary/objectives: {profile.about_user}</h4>
+      <iframe className={styles.resumeFrame} src={profile.user_resume} width="100%" height="250px"></iframe>
+      <button className={styles.updateButton} onClick={() => navigate("/profile-update/user/") }>Update Profile</button>
+    </div>
     </div>
   )
 }
 
-export default UserProfile
+export default UserProfile;
+
+
+
+
+
+
+
+
+
+
+// import React, { useEffect }from 'react'
+// import { useDispatch, useSelector} from 'react-redux';
+// import { getOneUser } from '../../store/user/userAction';
+// import { useNavigate } from 'react-router-dom';
+
+// const UserProfile = () => {
+//   const { profile } = useSelector(state => state.user)
+// console.log(profile);
+
+
+
+
+// const dispatch = useDispatch()
+// const navigate = useNavigate()
+
+// useEffect(() => {
+//   dispatch(getOneUser());
+// }, []);
+
+//   return (
+//     <div>
+//   <img src={profile.image} alt={profile.name} width="50" height="50" />
+//   <h3>User Name: {profile.name}</h3>
+//   <h4>User Surname: {profile.surname}</h4>
+//   <h4>User contact email: {profile.email}</h4>
+//   <h4>Summery/objectives: {profile.about_user}</h4>
+//   <iframe src={profile.user_resume} width="50%" height="250px"></iframe>
+//   <button onClick={() => navigate("/profile-update/user/") }>Update Profile</button>
+
+//     </div>
+//   )
+// }
+
+// export default UserProfile
--- src/components/profile/UserProfileUpdate.jsx
+++ src/components/profile/UserProfileUpdate.jsx
@@ -1,49 +1,53 @@
-import React, {useState, useEffect} from 'react'
-import {updateUserProfile,  getOneUser} from '../../store/user/userAction'
-import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { updateUserProfile, getOneUser } from '../../store/user/userAction';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import styles from './userProfileUpdate.module.css'; 
 
 const UserProfileUpdate = () => {
-  const {profile} = useSelector(state => state.user)
+  const { profile } = useSelector(state => state.user);
   const [editProfile, setEditProfile] = useState(profile);
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  console.log(editProfile);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(getOneUser());
-}, []);
+  }, []);
 
-useEffect(() => {
-  if (editProfile) {
-    setEditProfile(editProfile);
-  }
-}, [editProfile]);
+  useEffect(() => {
+    if (editProfile) {
+      setEditProfile(editProfile);
+    }
+  }, [editProfile]);
 
   return (
-    <div>
-    {profile && (
-      <>
-      <h3>Update Your Profile</h3>
-           <p>upload your profile image</p>
-            <input type="file" accept="image/*" onChange={(e) => setEditProfile({ ...editProfile, image: e.target.files[0] })} />
-
-            <input type="text" placeholder="Name" value={editProfile.name} onChange={(e) => setEditProfile({ ...editProfile, name: e.target.value })} />
-
-            <input type="text" placeholder="Surname" value={editProfile.surname} onChange={(e) => setEditProfile({ ...editProfile, surname: e.target.value })} />
-
-            <input type="email" placeholder="Contact email" value={editProfile.email} onChange={(e) => setEditProfile({ ...editProfile, email: e.target.value })} />
-           
-            <input type="text" placeholder="Summery" value={editProfile.about_user} onChange={(e) => setEditProfile({ ...editProfile, about_user: e.target.value })} />
-
-            <p>upload your resume in pdf</p>
-            <input type="file" accept=".pdf" placeholder="Resume" onChange={(e) => setEditProfile({ ...editProfile, user_resume: e.target.files[0] })} />
-            
-            <button onClick={() => dispatch(updateUserProfile({ editProfile, navigate }))}>Save profile changes</button>
-      </>
-    )}
-  </div>
+    <div className={styles.main}>
+    <div className={styles.container}>
+      {profile && (
+        <>
+          <h3 className={styles.heading}>Update Your Profile</h3>
+          <p className={styles.uploadLabel}>Upload your profile image</p>
+          <input type="file" accept="image/*" onChange={(e) => setEditProfile({ ...editProfile, image: e.target.files[0] })} className={styles.inputField} />
+          <input type="text" placeholder="Name" value={editProfile.name} onChange={(e) => setEditProfile({ ...editProfile, name: e.target.value })} className={styles.inputField} />
+          <input type="text" placeholder="Surname" value={editProfile.surname} onChange={(e) => setEditProfile({ ...editProfile, surname: e.target.value })} className={styles.inputField} />
+          <input type="email" placeholder="Contact email" value={editProfile.email} onChange={(e) => setEditProfile({ ...editProfile, email: e.target.value })} className={styles.inputField} />
+          <input type="text" placeholder="Summary" value={editProfile.about_user} onChange={(e) => setEditProfile({ ...editProfile, about_user: e.target.value })} className={styles.inputField} />
+          <p className={styles.uploadLabel}>Upload your resume in PDF</p>
+          <input type="file" accept=".pdf" onChange={(e) => setEditProfile({ ...editProfile, user_resume: e.target.files[0] })} className={styles.inputField} />
+          <button className={styles.button} onClick={() => dispatch(updateUserProfile({ editProfile, navigate }))}>Save profile changes</button>
+        </>
+      )}
+    </div>
+    </div>
   )
 }
 
-export default UserProfileUpdate
+export default UserProfileUpdate;
+
+
+
+
+
+
+
+
